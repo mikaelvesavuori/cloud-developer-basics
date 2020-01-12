a6782c283bc42f403a0b7924ff1603dcb9c64e15
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-01-12 13:32:57 +0100

    Minor updates to configuration (more RAM and add helper text about correct region if following serverless.yml demo)

--- 04-serverless-and-apis/backend/configuration.js
+++ 04-serverless-and-apis/backend/configuration.js
@@ -1,7 +1,7 @@
 // Fill these out for session 4
-exports.REGION = '';
 exports.PROJECT_ID = '';
 exports.TOPIC_NAME = '';
+exports.REGION = ''; // If you are OK with the region in serverless.yml, then set this to 'europe-west1'
 
 // Fill these out for session 5
 exports.BUCKET_NAME = '';
